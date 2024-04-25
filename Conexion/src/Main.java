c49bed8387adaac5c7a9bb8daae3d5149a0e24f6
John-zu <angel_neet@outlook.com>
2024-04-24 22:33:10 -0600

    ingreso de punto de guardado

--- Conexion/src/Main.java
+++ Conexion/src/Main.java
@@ -1,5 +1,21 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Connection connection = OracleDatabase.getConnection();
+
+        if (connection != null) {
+            System.out.println("Conexión exitosa a la base de datos Oracle");
+
+            // Aquí puedes realizar operaciones con la base de datos
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Error al conectar a la base de datos");
+        }
     }
 }
